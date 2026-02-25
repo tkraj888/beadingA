f9f990d386f3124ef7cf71442f485699ae0d06e1
tkraj888 <tusharck88@gmail.com>
2026-02-25 19:26:32 +0530

    all ok

--- src/main/java/com/spring/jwt/JwtWithSpringSecurityApplication.java
+++ src/main/java/com/spring/jwt/JwtWithSpringSecurityApplication.java
@@ -5,9 +5,10 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import java.util.TimeZone;
-
+@EnableAsync
 @SpringBootApplication
 @EnableScheduling
 @OpenAPIDefinition(info = @Info(title = "CarResell API'S", version = "1.0", description = "Api Documentation"))
--- src/main/java/com/spring/jwt/controller/DO/DOBidCarUploadController.java
+++ src/main/java/com/spring/jwt/controller/DO/DOBidCarUploadController.java
@@ -7,14 +7,24 @@ import com.spring.jwt.dto.ResponseDto;
 import com.spring.jwt.repository.IBidDoc;
 import com.spring.jwt.service.DOService;
 import lombok.RequiredArgsConstructor;
+import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -22,6 +32,8 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/uploadFileBidCar")
@@ -43,6 +55,7 @@ public class DOBidCarUploadController {
     private final String NODEJS_SERVER_URL = "https://digitaloceannodeimageservice-production.up.railway.app" ;
 
     private final RestTemplate restTemplate = new RestTemplate();
+    private final S3Client s3Client;
 
     @PostMapping("/addWithoutPhoto")
     public ResponseEntity<?> uploadWithoutImage(@RequestParam String documentType,@RequestParam String doc,@RequestParam String doctype,@RequestParam String subtype,@RequestParam String comment,@RequestParam Integer beadingCarId) throws InvalidKeyException, NoSuchAlgorithmException {
@@ -69,6 +82,49 @@ public class DOBidCarUploadController {
         }
 
     }
+//    @PostMapping("/add")
+//    public ResponseEntity<ResponceDto> uploadImage(
+//            @RequestParam("image") MultipartFile file,
+//            @RequestParam String documentType,
+//            @RequestParam String doc,
+//            @RequestParam String doctype,
+//            @RequestParam String subtype,
+//            @RequestParam String comment,
+//            @RequestParam Integer beadingCarId) {
+//
+//        System.out.println("Uploading image for car ID: " + beadingCarId + ", size: " + file.getSize());
+//
+//        try {
+//            // 1. Upload to R2 bucket
+//            String key = r2StorageService.uploadFile(file);
+//
+//            // 2. Build public link (R2 public URL format)
+//            String documentLink = "https://photos.caryanamindia.com/" + key;
+//
+//            // 3. Create DTO and populate all fields
+//            BidCarDto documentDto = new BidCarDto();
+//            documentDto.setComment(comment);
+//            documentDto.setDoctype(doctype);
+//            documentDto.setSubtype(subtype);
+//            documentDto.setDoc(doc);
+//            documentDto.setBeadingCarId(beadingCarId);
+//            documentDto.setDocumentType(documentType);
+//            documentDto.setDocumentLink(documentLink);
+//
+//            // 4. Save metadata in DB
+//            String serviceResponse = iDocument.addDocument(documentDto);
+//
+//            return ResponseEntity.ok(new ResponceDto("success", serviceResponse));
+//
+//        } catch (RuntimeException e) {
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+//                    .body(new ResponceDto("unsuccess", e.getMessage()));
+//        } catch (Exception e) {
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                    .body(new ResponceDto("unsuccess", "Failed to upload image: " + e.getMessage()));
+//        }
+//    }
+
     @PostMapping("/add")
     public ResponseEntity<ResponceDto> uploadImage(
             @RequestParam("image") MultipartFile file,
@@ -79,39 +135,14 @@ public class DOBidCarUploadController {
             @RequestParam String comment,
             @RequestParam Integer beadingCarId) {
 
-        System.out.println("Uploading image for car ID: " + beadingCarId + ", size: " + file.getSize());
+        r2StorageService.compressUploadAndSave(
+                file, documentType, doc, doctype, subtype, comment, beadingCarId
+        );
 
-        try {
-            // 1. Upload to R2 bucket
-            String key = r2StorageService.uploadFile(file);
-
-            // 2. Build public link (R2 public URL format)
-            String documentLink = "https://photos.caryanamindia.com/" + key;
-
-            // 3. Create DTO and populate all fields
-            BidCarDto documentDto = new BidCarDto();
-            documentDto.setComment(comment);
-            documentDto.setDoctype(doctype);
-            documentDto.setSubtype(subtype);
-            documentDto.setDoc(doc);
-            documentDto.setBeadingCarId(beadingCarId);
-            documentDto.setDocumentType(documentType);
-            documentDto.setDocumentLink(documentLink);
-
-            // 4. Save metadata in DB
-            String serviceResponse = iDocument.addDocument(documentDto);
-
-            return ResponseEntity.ok(new ResponceDto("success", serviceResponse));
-
-        } catch (RuntimeException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(new ResponceDto("unsuccess", e.getMessage()));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ResponceDto("unsuccess", "Failed to upload image: " + e.getMessage()));
-        }
+        return ResponseEntity.ok(
+                new ResponceDto("success", "File is uploading in background…")
+        );
     }
-
     @DeleteMapping("/delete")
     private ResponseEntity<?> delete(@RequestParam Integer DocumentId) {
         try {
@@ -190,4 +221,47 @@ public class DOBidCarUploadController {
 
         }
     }
+    public File compressImage(MultipartFile file) throws IOException {
+
+        String fileName = UUID.randomUUID() + ".jpg";
+        File compressedFile = new File(System.getProperty("java.io.tmpdir"), fileName);
+
+        // Convert MultipartFile → BufferedImage
+        BufferedImage originalImage = ImageIO.read(file.getInputStream());
+
+        int targetWidth = 1280;  // Resize for BIG compression
+        int targetHeight = (originalImage.getHeight() * 1280) / originalImage.getWidth();
+
+        // Resize + 40% JPEG quality
+        Thumbnails.of(originalImage)
+                .size(targetWidth, targetHeight)
+                .outputFormat("jpg")
+                .outputQuality(0.4)      // 40% quality gives ~1–2MB
+                .toFile(compressedFile);
+
+        return compressedFile;
+    }
+    public File compressVideo(MultipartFile file) throws Exception {
+
+        File inputFile = File.createTempFile("input_", ".mp4");
+        file.transferTo(inputFile);
+
+        File outputFile = File.createTempFile("compressed_", ".mp4");
+
+        String cmd = String.format(
+                "ffmpeg -y -i %s -vcodec libx264 -crf 28 %s",
+                inputFile.getAbsolutePath(),
+                outputFile.getAbsolutePath()
+        );
+
+        Process process = Runtime.getRuntime().exec(cmd);
+        process.waitFor();
+
+        return outputFile;
+    }
+
+
+
+
+
 }
